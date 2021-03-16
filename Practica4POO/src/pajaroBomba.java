7d7ef6dfda3636041261e23f5d486e73ba690987
AliGaxiola <79878459+AliGaxiola@users.noreply.github.com>
2021-03-15 23:17:14 -0700

    Add files via upload

---
+++ Practica4POO/src/pajaroBomba.java
@@ -0,0 +1,13 @@
+import java.util.concurrent.ThreadLocalRandom;
+public class pajaroBomba extends Pajaro{
+
+    public pajaroBomba(String color, String form, int damage){
+        super(color, form, damage);
+    }
+
+    public void Explosion(){
+        int num = ThreadLocalRandom.current().nextInt(500, 1000 + 1);
+        System.out.println("El pajaro bomba exploto y genero un daño de: "+num);
+    }
+
+}
