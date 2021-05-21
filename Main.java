c8a03191ecd952f404990636209515d1611f85a3
AliGaxiola <79878459+AliGaxiola@users.noreply.github.com>
2021-05-20 17:54:19 -0700

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main{
-    public static void main(String[] args){
-        System.out.println("Torneo entre ligas de USA \n");
-        Torneo torneo  = new Torneo("Grandes ligas", "Americana", 5, 5,2);
-        torneo.agregarEquipos();
-        System.out.println("\n");
-        torneo.datosTorneo();
-    }
-}
